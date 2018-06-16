fb5346749468d0a38d1f99acf08923a744d0d141
Buk1m <bkudra@hotmail.com>
2018-06-16 17:55:45 +0200

    initial commit

---
+++ ModemTalking/Model/ModemTalking.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Windows;
+
+namespace ModemTalking.Model
+{
+    /// <summary>
+    /// Model do komunikacji za pomoca modemów
+    /// </summary>
+    class ModemTalking
+    {
+        // zmienna typu SeialPort do obslugi portu szeregowego
+        internal SerialPort MyHeart;
+
+        /// <summary>
+        /// Konstruktor ModemTalking
+        /// </summary>
+        /// <param name="portName"> nazwa portu ktory ma zaostac otwarty </param>
+        /// <param name="baudrate"> baudrate - predkosc komunikacji </param>
+        public ModemTalking(string portName, int baudrate)
+        {
+            // ustawienia portu
+            MyHeart = new SerialPort
+            {
+                // nazwa portu
+                PortName = portName,
+                // predkosc komuniacji
+                BaudRate = baudrate,
+                // timeout dla odczytu z portu
+                ReadTimeout = 500,
+                // timeout dla nadawania przez port
+                WriteTimeout = 500,
+                // ustawia sygnal Data Terminal Ready
+                DtrEnable = true,
+                // wskazuje czy wlaczony jest sygnal Request to send
+                RtsEnable = true,
+                // liczba bitów w bajcie
+                DataBits = 8,
+                // typ inicjalizacji polaczenia
+                Handshake = Handshake.None,
+                // bit parzystosci
+                Parity = Parity.None,
+                // bit stopu
+                StopBits = StopBits.One,
+                // znak nowej linii dla WriteLine i ReadLine
+                NewLine = "\r"
+            };
+
+            //Otwarcie portu
+            MyHeart.Open();
+        }
+
+        /// <summary>
+        /// Funkcja wywylajaca dane przez port
+        /// </summary>
+        /// <param name="message"> wiadomosc do przeslania </param>
+        public void Send(string message)
+        {
+            // wpisz linie z wiadomoscia
+            MyHeart.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Funkcja odczytujaca zbuferowane dane z portu
+        /// </summary>
+        /// <returns></returns>
+        public string Read()
+        {
+            try
+            {
+                // sprawdzenie liczby bajtów w buforze i utworzenie tablicy o takim rozmiarze
+                byte[] packOfBytes = new byte[MyHeart.BytesToRead];
+                // odczytanie oczekujacych w buforze bajtów
+                _mySoul = Encoding.ASCII.GetString(packOfBytes, 0, MyHeart.Read(packOfBytes, 0, packOfBytes.Length));
+            }
+            catch (TimeoutException)
+            {
+                // jezeli nastapi timeout to poinformuj uzytkownika
+                MessageBox.Show("Timeout");
+            }
+
+            // zwroc wiadomosc
+            return _mySoul;
+        }
+
+
+        /// <summary>
+        /// Metoda zamykajaca port i zwalniajaca zasoby
+        /// </summary>
+        public void Close()
+        {
+            // zwolnienie zasobow
+            MyHeart.Dispose();
+        }
+
+        // zmienna przechowywujaca odebana wiadomosc
+        private string _mySoul = "";
+
+    }
+
+}
---
+++ ModemTalking/ViewModel/MainViewModel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Ports;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using ModemTalking.Commands;
+using ModemTalking.Model;
+
+namespace ModemTalking.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        private Model.ModemTalking _modemTalking;
+        private string _selectedCom;
+        private string _senderMessage;
+        private string _reciverMessage;
+        private int _selectedBaudrate = 9600;
+
+        #endregion
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            OpenFile = new RelayCommand(() =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog { Filter = "Text files (*.txt)|*.txt" };
+                if (openFileDialog.ShowDialog() != true)
+                    return;
+
+                SenderTextBox = File.ReadAllText(openFileDialog.FileName);
+            });
+
+            Send = new RelayCommand(() =>
+            {
+                if (_modemTalking == null || _selectedCom == null)
+                {
+                    MessageBox.Show("Choose serial port.");
+                    return;
+                }
+
+                if (SenderTextBox == null)
+                {
+                    MessageBox.Show("Enter message to be sent.");
+                    return;
+                }
+
+                _modemTalking.Send(SenderTextBox);
+            });
+
+            RefreshPorts = new RelayCommand(() =>
+            {
+                _modemTalking?.Close();
+                _selectedCom = null;
+                ComPorts?.Clear();
+                foreach (var portName in SerialPort.GetPortNames())
+                {
+                    ComPorts?.Add(portName);
+                }
+            });
+
+            ClosePorts = new RelayCommand(() => { _modemTalking?.Close(); });
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public ObservableCollection<string> ComPorts { get; set; } =
+            new ObservableCollection<string>(SerialPort.GetPortNames());
+
+        public ObservableCollection<string> Baudrate { get; set; } =
+            new ObservableCollection<string>() { "9600", "115200" };
+
+        public string SelectedSenderBaudrate
+        {
+            get => _selectedBaudrate.ToString();
+            set
+            {
+                _selectedBaudrate = int.Parse(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private void Recive(object sender, EventArgs e)
+        {
+            ReciverTextBox = _modemTalking.Read();
+        }
+
+        public string SelectedSenderCom
+        {
+            get => _selectedCom;
+            set
+            {
+                if (_selectedCom == value || value == null)
+                    return;
+
+                _selectedCom = value;
+                try
+                {
+                    _modemTalking?.Close();
+                    _modemTalking = new Model.ModemTalking(_selectedCom, _selectedBaudrate);
+                    _modemTalking.MyHeart.DataReceived += Recive;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Port is being used by other process. Acces denied.");
+                    OnPropertyChanged();
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string SenderTextBox
+        {
+            get => _senderMessage;
+
+            set
+            {
+                _senderMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ReciverTextBox
+        {
+            get => _reciverMessage;
+
+            set
+            {
+                _reciverMessage += value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenFile { get; set; }
+        public ICommand Send { get; set; }
+        public ICommand RefreshPorts { get; set; }
+        public ICommand ClosePorts { get; set; }
+
+        #endregion
+    }
+
+}
